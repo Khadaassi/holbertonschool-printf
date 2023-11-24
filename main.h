fe5217965a13239231267bf921af96847588f7df
khadouj <aassikhadija@gmail.com>
2023-11-24 15:06:51 +0100

    add main.h file

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+int get_function(const char specifier, va_list arguments);
+int _putchar(char character);
+int print_character(va_list arguments);
+int print_string(va_list arguments);
+int print_percentage(va_list arguments);
+int print_integer(va_list arguments);
+
+/**
+ * struct format_handler_structure - structure to handle format specifiers
+ * and corresponding functions
+ * @specifier: the format specifier character
+ * @function: a function pointer to the corresponding data processing function
+ */
+typedef struct format_handler_structure
+
+{
+	char specifier;
+	int (*function)(va_list);
+} format_handler;
+
+#endif
